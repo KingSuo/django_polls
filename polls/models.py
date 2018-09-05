5ff2b4fe3f5df4beff2c8ea019d945492cf1fd6a
KingSuo <648230502@qq.com>
2018-09-06 00:51:01 +0800

    finish lesson1 & 2 & half 3

--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,9 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question_text
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question)
+    question = models.ForeignKey(Question, related_name="choices")
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, api_views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # API views
+    url(r'^api/questions/$', api_views.QuestionList.as_view()),
+    url(r'^api/questions/(?P<pk>[0-9]+)/$', api_views.QuestionDetail.as_view()),
+    url(r'^api/choices/(?P<pk>[0-9]+)/$', api_views.ChoiceDetail.as_view()),
 ]
